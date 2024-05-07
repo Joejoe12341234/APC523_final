73ccef10b86308f58fb1192a11bfb95f7f50a1a8
mr-gabrielrios <mr.gabrielrios@gmail.com>
2024-05-07 13:00:58 -0400

    Improved utility functionality.

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ import argparse
 from run import initialize, integrate
 from utils import storage, helper, math, solver, visualization
 
+def check_positive(value):
+    # Credit: user "Yuushi" on StackOverflow at: https://stackoverflow.com/questions/14117415/how-can-i-constrain-a-value-parsed-with-argparse-for-example-restrict-an-integ
+    ivalue = float(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+
 def run_model(N=50, U=10, nu=0.1, cfl=0.25, runtime=0.1, timestep_method='projection', spatial_method='cdf2', visualize=False, save_data=False):
 
     ''' 1. Derive values needed to run initialization. '''
@@ -48,14 +55,16 @@ def run_model(N=50, U=10, nu=0.1, cfl=0.25, runtime=0.1, timestep_method='projec
 ''' Argument parsing. '''
 # Set up intake of arguments from the command line
 parser = argparse.ArgumentParser()
-parser.add_argument('--grid_size')
-parser.add_argument('--flow_speed')
-parser.add_argument('--viscosity')
-parser.add_argument('--CFL')
-parser.add_argument('--model_time')
-parser.add_argument('--timestep_method')
-parser.add_argument('--spatial_discretization')
+parser.add_argument('--grid_size', type=check_positive)
+parser.add_argument('--flow_speed', type=check_positive)
+parser.add_argument('--viscosity', type=check_positive)
+parser.add_argument('--CFL', type=check_positive)
+parser.add_argument('--model_time', type=check_positive)
+parser.add_argument('--timestep_method', type=check_positive)
+parser.add_argument('--spatial_discretization', type=check_positive)
 parser.add_argument('--plots')
+parser.add_argument('--save_data')
+
 # Define variables from inputs
 args = parser.parse_args()
 N = args.grid_size if args.grid_size else 50
@@ -66,6 +75,7 @@ runtime = args.model_time if args.model_time else 0.75
 timestep_method = args.timestep_method if args.timestep_method else 'projection'
 spatial_method = args.spatial_discretization if args.spatial_discretization else 'cdf2'
 visualize = args.plots if args.plots else True
+save_data = args.save_data if args.save_data else True
 
 # Run the model with user inputs
-run_model(N, U, nu, cfl, runtime, timestep_method, spatial_method, visualize)
+run_model(N, U, nu, cfl, runtime, timestep_method, spatial_method, visualize, save_data)
